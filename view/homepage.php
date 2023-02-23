5e296b6e19a6b752fb9e9c3210a39b45604dd934
JesusCAN3LON <64654517+JesusCAN3LON@users.noreply.github.com>
2023-02-23 10:17:41 -0400

    creando pagina F1

--- view/homepage.php
+++ view/homepage.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,7 +9,131 @@
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/styles.css">
 </head>
-<body>
-    <h1>heeeyyyy</h1>
+
+<body class="fondoHome">
+    <div class="container-fluid">
+
+        <!-- HEADER -->
+        <div class="row">
+            <header class="text-white p-5">
+                <div class="d-flex justify-content-between">
+                    <div class="d-flex">
+                        <img class="me-5" src="assets/icons/F1.png" width="auto" height="40" alt="">
+                        <ul class="nav">
+                            <li class="m-2">Equipos</li>
+                            <li class="m-2">Corredores</li>
+                            <li class="m-2">Mercancia</li>
+                            <li class="m-2">Pistas</li>
+                        </ul>
+                    </div>
+                    <div class="d-flex">
+                        <div class="me-3">
+                            <input type="search" class="form-control" placeholder="Buscar..." aria-label="Search">
+                        </div>
+                        <div>
+                            <a class="btn btn-danger" href="#">Cerrar sesion</a>
+                        </div>
+                        <div>
+                            <a class="btn btn-danger" href="#">Cerrar sesion</a>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </div>
+        <!-- FIN DE HEADER -->
+
+
+        <div class="row d-flex justify-content-center mt-4 mb-4">
+            <div class="col-5">
+                <div class="card noticia1">
+                    <div class="card-body">
+                        <h2 class="ph2">EMPEZO LA FORMULA 1: Ya esta cerca la nueva temporada de la F1.</h2>
+                        <div class="mb-2">
+                            <img class="img-fluid" src="assets/img/mercedes-w14.jpg" alt="">
+                        </div>
+                        <p class="pHome">Alucina con el nuevo monoplaza creado por la escuderia de Mercedes AMG. Llamado asi el Mercedes W14.</p>
+                        <a class="btn btn-outline-danger text-decoration-none text-white" href="#">Leer mas sobre el W14</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card noticia2">
+                    <div class="card-body">
+                        <div class="mb-2">
+                            <img class="img-fluid" src="assets/img/lewis.jpg" alt="">
+                        </div>
+                        <span class="text-danger small fst-italic">NEWS</span>
+                        <p class="pHome">Lewis Hamilton estaria preparandose estos dias para las primeras pruebas con el W14</p>
+                    </div>
+                </div>
+                <div class="card noticia2">
+                    <div class="card-body">
+                        <div class="mb-2">
+                            <img class="img-fluid" src="assets/img/lewis.jpg" alt="">
+                        </div>
+                        <span class="text-danger small fst-italic">NEWS</span>
+                        <p class="pHome">Lewis Hamilton estaria preparandose estos dias para las primeras pruebas con el W14</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card noticia2">
+                    <div class="card-body">
+                        <div class="mb-2">
+                            <img class="img-fluid" src="assets/img/lewis.jpg" alt="">
+                        </div>
+                        <span class="text-danger small fst-italic">NEWS</span>
+                        <p class="pHome">Lewis Hamilton estaria preparandose estos dias para las primeras pruebas con el W14</p>
+                    </div>
+                </div>
+                <div class="card noticia2">
+                    <div class="card-body">
+                        <div class="mb-2">
+                            <img class="img-fluid" src="assets/img/lewis.jpg" alt="">
+                        </div>
+                        <span class="text-danger small fst-italic">NEWS</span>
+                        <p class="pHome">Lewis Hamilton estaria preparandose estos dias para las primeras pruebas con el W14</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+        <!-- FOOTER -->
+        <div class="row bg-dark text-white p-4">
+            <div class="col-xs-12 col-lg-3">
+                <h4>Codigo Can3loN</h4>
+            </div>
+            <div class="col-xs-12 col-lg-3">
+                <h5 class="mb-4">Links</h5>
+                <div class="mb-2">
+                    <a class="text-decoration-none text-white" href="">Terminos y condiciones</a>
+                </div>
+                <div>
+                    <a class="text-decoration-none text-white" href="">Privacidad y politica</a>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-lg-3">
+                <h5>Codigo escrito por Jesus Canelon</h5>
+            </div>
+
+            <div class="col-xs-12 col-lg-3">
+                <h5 class="mb-4">Contactos</h5>
+                <div class="mb-2">
+                    <a class="text-decoration-none text-white" href="">Instagram</a>
+                </div>
+                <div class="mb-2">
+                    <a class="text-decoration-none text-white" href="">Youtube</a>
+                </div>
+                <div class="mb-2">
+                    <a class="text-decoration-none text-white" href="">Facebook</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
 </body>
+
 </html>
